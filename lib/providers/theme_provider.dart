a005ad4665c88bd37f96875b19f7a1d30788a0e9
ishantbhurani <67356291+ishantbhurani@users.noreply.github.com>
2024-04-27 19:52:41 +0530

    persist app theme in sharedpref

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  Brightness _brightness = Brightness.light;
+  static late final SharedPreferences prefs;
+
+  static late Brightness _brightness;
+
+  static const themePrefKey = 'isDarkTheme';
+
+  static Future<void> init() async {
+    prefs = await SharedPreferences.getInstance();
+    final isDarkTheme = prefs.getBool(themePrefKey) ?? false;
+    _brightness = isDarkTheme ? Brightness.dark : Brightness.light;
+  }
 
   Brightness get brightness => _brightness;
 
   set brightness(Brightness brightness) {
     _brightness = brightness;
+    prefs.setBool(themePrefKey, isDarkMode());
     notifyListeners();
   }
 
